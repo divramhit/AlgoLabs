39f78cd2327dfc1fc3e7e768a1eff28337489642
divramhit <68056928+divramhit@users.noreply.github.com>
2022-06-10 17:18:59 +0400

    ques1 Start

--- lab3/ques1/Main.java
+++ lab3/ques1/Main.java
@@ -1,10 +1,13 @@
+//Given an array of integers containing duplicates, return the majority element in the array, if present. A
+//majority element appears more than n/2 times where n is the size of the array.
+//For example: the majority element is 2 in the array {2, 8, 7, 2, 2, 5, 2, 3, 1, 2, 2}.
 package ques1;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] arrayNum = {2, 8, 7, 2, 2, 5, 2, 3, 1, 2, 2};
 	}
 
 }
