8199293801a3a21e6167b78f16060a21f10d3935
divramhit <68056928+divramhit@users.noreply.github.com>
2022-07-21 03:16:23 +0400

    Started LAB18 Question, added fileReader and testing each line with bruteForce Algorithms. Need to have a counter for number of comparisons and total comparisons

--- lab18_PatternMatching/mainPatternMatching.java
+++ lab18_PatternMatching/mainPatternMatching.java
@@ -1,7 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class mainPatternMatching {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
 		String str = "AAABBAABBABABAABBABAB";
@@ -23,7 +27,8 @@ public class mainPatternMatching {
 		BoyerMooreSearch.search(str, pattern);
 		System.out.println("");
 		
-		//Labsheet18 Question
+//		Labsheet18 Question Start
+		
 //		Write a simple program (in JAVA) that reads a small text file and applies the following string searching
 //		algorithms to search for text inputs from the keyboard.
 //		i. Brute-force searching
@@ -31,6 +36,30 @@ public class mainPatternMatching {
 //		iii. Boyer-Moore searching
 //		You are also required to count the number of comparisons (for the same text file and the same search
 //		pattern) done in each case and display same in a tabular format.
+		
+		pattern = "QPNE";	
+		int lineCounter = 0;
+		
+		try {
+			BufferedReader filereader = new BufferedReader(new FileReader("C:\\Users\\Ramhit\\eclipse-workspace\\2019Y Lab\\lab18_PatternMatching\\patternMatching.txt"));
+			
+			String line;
+			
+			while((line = filereader.readLine()) != null) {
+				lineCounter++;
+				System.out.print("Line :" + lineCounter + " ");
+				bruteForceSearch.search(line, pattern);
+				
+				//Change the Search Algorithms to enable adding and fit this question add a function to return number of times matching occurred
+				//Need to account for number of comparisons done as well 
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error Occurred: ");
+			e.printStackTrace();
+		}
 		
 		
 		
